4c8d5dd5fe1b02dde7e4c520bf1cda159e371531
Anthony Duong <khangbeo2012@gmail.com>
2022-09-03 07:34:12 -0700

    changed card transition from 300 to 200

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -2,7 +2,7 @@ export default function AnimeCard({ anime }) {
   return (
     <>
       <article className="mx-auto w-full md:w-56 lg:w-64">
-        <div className="mb-2 card ease-in-out duration-300 hover:scale-105 hover:shadow-2xl cursor-pointer">
+        <div className="mb-2 card ease-in-out duration-200 hover:scale-105 hover:shadow-2xl cursor-pointer">
           <figure>
             <img
               className="object-cover h-full w-full md:h-96"
